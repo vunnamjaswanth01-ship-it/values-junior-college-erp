4023ed84fca69566658468b9c014734e19ee63d1
vunnamjaswanth01-ship-it <vunnamjaswanth01@gmail.com>
2026-06-26 03:51:05 +0530

    Restore previous working UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,1 @@
-import React from 'react';import{createRoot}from'react-dom/client';import'./styles.css';import App from'./App';createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+import React from 'react';import{createRoot}from'react-dom/client';import'./styles.css';import App from'./App.jsx';createRoot(document.getElementById('root')).render(<App/>);
